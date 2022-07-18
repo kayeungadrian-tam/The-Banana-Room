1d41ebceb5ffbc7e1fc40fda2ad22433a4b340d3
AIF-adrian <tamkayeung.adrian@gmail.com>
2022-07-18 18:01:17 +0900

    🚀 add animation

--- the_app/lib/src/core/constants/lotties.dart
+++ the_app/lib/src/core/constants/lotties.dart
@@ -5,3 +5,5 @@ const likeNoBackground = 'assets/lotties/like-no-background.json';
 const rocket = 'assets/lotties/rocket.json';
 const profile = 'assets/lotties/profile.json';
 const home = 'assets/lotties/home.json';
+const gmae_contorller = 'assets/lotties/game_controller.json';
+const door = "assets/lotties/door.json";
